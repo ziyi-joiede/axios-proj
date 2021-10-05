3e953b18f23230ae7c3e9d5b09b1afbe355dd529
YoNgWeI <30373219+YoNgWeI1992@users.noreply.github.com>
2021-10-06 07:49:28 +0800

    处理 request 请求 body 部分的数据

--- examples/base/app.ts
+++ examples/base/app.ts
@@ -70,55 +70,10 @@ axios({
   }
 })
 
-axios({
-  method: 'post',
-  url: '/base/post',
-  headers: {
-    'content-type': 'application/json;charset=utf-8'
-  },
-  data: {
-    a: 1,
-    b: 2
-  }
-})
-
 const arr = new Int32Array([21, 31])
 
 axios({
   method: 'post',
-  url: '/base/buffer',
+  url:'/base/buffer',
   data: arr
 })
-
-
-const paramsString = 'q=URLUtils.searchParams&topic=api'
-const searchParams = new URLSearchParams(paramsString)
-
-axios({
-  method: 'post',
-  url: '/base/post',
-  data: searchParams
-})
-
-// axios({
-//   method: 'post',
-//   url: '/base/post',
-//   data: {
-//     a: 1,
-//     b: 2
-//   }
-// }).then((res) => {
-//   console.log(res)
-// })
-
-// axios({
-//   method: 'post',
-//   url: '/base/post',
-//   responseType: 'json',
-//   data: {
-//     a: 3,
-//     b: 4
-//   }
-// }).then((res) => {
-//   console.log(res)
-// })
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { AxiosReuestConfig } from './types'
 import xhr from './xhr'
 import { buildURL } from './helpers/url'
+import { transformRequest } from './helpers/data'
 
 function axios(config: AxiosReuestConfig):void {
   processConfig(config)
@@ -8,13 +9,27 @@ function axios(config: AxiosReuestConfig):void {
   xhr(config)
 }
 
+/**
+ * @description 处理 config
+ * @param { AxiosReuestConfig } config 请求的配置参数
+ */
 function processConfig(config: AxiosReuestConfig):void {
   config.url = transformURL(config)
+  config.data = transformRequestData(config)
 }
 
+/**
+ * @description 转换 url
+ * @param { AxiosReuestConfig } config 请求的配置参数
+ * @returns
+ */
 function transformURL(config: AxiosReuestConfig):string {
   const { url, params } = config
   return buildURL(url, params)
+}
+
+function transformRequestData(config: AxiosReuestConfig):any {
+  return transformRequest(config.data)
 }
 
 export default axios
